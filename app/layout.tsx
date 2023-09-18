e9bba204d60118266430d08319822fa37876592b
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-18 21:22:10 +0530

    project initial setup 🗒️

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'JSM 2.0',
@@ -16,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className='min-h-screen bg-black-100'>{children}</body>
     </html>
   )
 }
