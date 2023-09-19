77a5ac13d5cb2d0769b1f2fd9940b069f036865b
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-19 20:29:35 +0530

    Navbar 🔗

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex-center fixed top-0 z-50 w-full border-b-2 bg-black-200 py-7 text-white">
+      <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
+        <Link href={"/"}>
+          <Image src={"/jsm-logo.svg"} alt="app-logo" width={55} height={40} />
+        </Link>
+        <Image
+          className="block md:hidden"
+          src={"/hamburger-menu.svg"}
+          alt="menu"
+          width={30}
+          height={30}
+        />
+        <ul className="flex-center gap-x-3 max-md:hidden md:gap-x-10">
+          <li className="body-text text-gradient_blue-purple !font-bold">
+            <Link href={"/"}>Next.js 13.4</Link>
+          </li>
+          <li className="body-text  !font-normal">
+            <Link href={"/"}>About us</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
