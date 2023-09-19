c7b6f362689346930d73f7b7d156c2297edcbdfa
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-19 20:55:01 +0530

    improved 🗒️

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -7,12 +7,16 @@ const links = ["all", "Next 13", "frontend", "backend", "fullstack"];
 const Filters = () => {
   const [active, setActive] = useState("");
 
+  const handleFilter = (link: string) => {
+    setActive(link);
+  };
+
   return (
     <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
       {links.map((link) => (
         <button
           key={link}
-          onClick={() => {}}
+          onClick={() => handleFilter(link)}
           className={`${
             active === link ? "gradient_blue-purple" : ""
           } whitespace-nowrap rounded-lg px-8 py-2.5 capitalize`}
