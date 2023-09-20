fd55a1ce1017a27e974db674f29499c02ba4d9e5
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-20 20:33:49 +0530

    debounce search func ⭕

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -2,10 +2,40 @@
 
 import Image from "next/image";
 import { Input } from "./ui/input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { formUrlQuery } from "@/sanity/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const SearchForm = () => {
   const [search, setSearch] = useState("");
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const delayDebounceFunc = setTimeout(() => {
+      let newUrl = "";
+
+      console.log(search);
+      if (search) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: search,
+        });
+      } else {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFunc);
+  }, [search]);
 
   return (
     <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
