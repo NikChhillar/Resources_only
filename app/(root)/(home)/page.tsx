c0c619d1671b0c01ea1ec986cedd574c2bd6767f
Nikhil <90024300+NikChhillar@users.noreply.github.com>
2023-09-20 11:10:33 +0530

    get resources with categories 🔗

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,21 @@
 import Filters from "@/components/Filters";
+import Header from "@/components/Header";
 import ResourceCard from "@/components/ResourceCard";
 import SearchForm from "@/components/SearchForm";
 import { getResources } from "@/sanity/actions";
 import React from "react";
 
-const HomePage = async () => {
+export const revalidate = 600;
+
+interface Props {
+  searchParams: { [key: string]: string | undefined };
+}
+
+const HomePage = async ({ searchParams }: Props) => {
+  console.log(searchParams);
   const resources = await getResources({
     query: "",
-    category: "",
+    category: searchParams?.category || "",
     page: "1",
   });
 
@@ -31,7 +39,7 @@ const HomePage = async () => {
       {/*  */}
 
       <section className="flex-center mt-6 w-full flex-col sm:mt-20">
-        Header...
+        <Header />
         <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
           {resources?.length > 0 ? (
             resources.map((resource: any) => (
